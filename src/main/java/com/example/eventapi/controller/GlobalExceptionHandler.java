45a172f20d461d86f016c549790a13a81527ac0a
Svitlana <LanaLubavskaya@gmail.com>
2026-05-31 12:16:24 +0300

    update error handler

--- src/main/java/com/example/eventapi/controller/GlobalExceptionHandler.java
+++ src/main/java/com/example/eventapi/controller/GlobalExceptionHandler.java
@@ -6,7 +6,6 @@ import com.example.eventapi.dto.ApiError;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
